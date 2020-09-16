cdf446d2477105eab9edbc35df6d4ec0cf8a3ea9
Livingstone <43893458+lividavi19@users.noreply.github.com>
2020-09-16 09:04:07 +0300

    Update async-fetch.js

--- async-fetch.js
+++ async-fetch.js
@@ -2,13 +2,29 @@
 async function _get (url, successCallback, failCallback) {
 	try {
 		const response = await fetch(url);
+		const textData = await response.text();
+		const jsonData = await response.json();
+		const blobData = await response.blob();
+		
 		if (response.ok) {
-			successCallback(response);
+			successCallback({
+				success: true,
+				response: response,
+				text: textData,
+				json: jsonData,
+				blob: blobData
+			});
 		} else {
-			failCallback(response);
+			failCallback({
+				success: false,
+				response: response
+			});
 		}
 	} catch (error) {
-		failCallback(error);
+		failCallback({
+			success: false,
+			response: error
+		});
 	}
 }
 
@@ -16,13 +32,29 @@ async function _get (url, successCallback, failCallback) {
 async function _post (url, body, successCallback, failCallback) {
 	try {
 		const response = await fetch(url, {method: `post`, body: body});
+		const textData = await response.text();
+		const jsonData = await response.json();
+		const blobData = await response.blob();
+
 		if (response.ok) {
-			successCallback(response);
+			successCallback({
+				success: true,
+				response: response,
+				text: textData,
+				json: jsonData,
+				blob: blobData
+			});
 		} else {
-			failCallback(response);
+			failCallback({
+				success: false,
+				response: response
+			});
 		}
 	} catch (error) {
-		failCallback(error);
+		failCallback({
+			success: false,
+			response: error
+		});
 	}
 }
 
@@ -30,13 +62,29 @@ async function _post (url, body, successCallback, failCallback) {
 async function _put (url, body, successCallback, failCallback) {
 	try {
 		const response = await fetch(url, {method: `put`, body: body});
+		const textData = await response.text();
+		const jsonData = await response.json();
+		const blobData = await response.blob();
+
 		if (response.ok) {
-			successCallback(response);
+			successCallback({
+				success: true,
+				response: response,
+				text: textData,
+				json: jsonData,
+				blob: blobData
+			});
 		} else {
-			failCallback(response);
+			failCallback({
+				success: false,
+				response: response
+			});
 		}
 	} catch (error) {
-		failCallback(error);
+		failCallback({
+			success: false,
+			response: error
+		});
 	}
 }
 
@@ -44,12 +92,28 @@ async function _put (url, body, successCallback, failCallback) {
 async function _delete (url, body, successCallback, failCallback) {
 	try {
 		const response = await fetch(url, {method: `delete`, body: body});
+		const textData = await response.text();
+		const jsonData = await response.json();
+		const blobData = await response.blob();
+
 		if (response.ok) {
-			successCallback(response);
+			successCallback({
+				success: true,
+				response: response,
+				text: textData,
+				json: jsonData,
+				blob: blobData
+			});
 		} else {
-			failCallback(response);
+			failCallback({
+				success: false,
+				response: response
+			});
 		}
 	} catch (error) {
-		failCallback(error);
+		failCallback({
+			success: false,
+			response: error
+		});
 	}
 }
