c692a5fb2cab441c713445d0dcbbcadd0f2adcce
Livingstone <43893458+lividavi19@users.noreply.github.com>
2023-08-13 22:33:47 +0300

    Options object parameter
    This change introduces the ability to pass in the Options object parameter for each utility function. It also reorders the function's parameters where resource URL is the first parameter, then comes the Options object, then successCallback, and finally failureCallback

--- async-fetch.js
+++ async-fetch.js
@@ -1,81 +1,109 @@
-// TODO: add request-headers support for each of the following methods
+/*
+	Written by Livingstone
+*/
 
-// get data
-const getData = async (url, successCallback, failCallback) => {
+/* TODO: for each of the utility functions below, handle data output based on Content-Type
+	Check response Content-Type
+	const contentType = response.headers.get('Content-Type');
+	let responseData;
+	if (contentType && contentType.includes('application/json')) {}
+*/
+
+// Get data
+const getData = async (url, options, successCallback, failCallback) => {
 	try {
-		const response = await fetch(url);
-		
+		const response = await fetch(url, {
+			headers : options.headers,
+			body : options.body
+		});
+
+		// Check if response is ok
 		if (response.ok) {
-			successCallback ({
-				response: response,
-				text: await response.clone().text(),
-				json: await response.clone().json(),
-				blob: await response.clone().blob()
+			successCallback({
+				response,
+				json : await response.clone().json(),
+				text : await response.clone().text(),
+				blob : await response.blob()
 			});
 		} else {
-			failCallback (response);
+			failCallback(response);
 		}
 	} catch (error) {
-		failCallback (error);
+		failCallback(error);
 	}
 };
 
-// post data
-const postData = async (url, body, successCallback, failCallback) => {
+// Post data
+const postData = async (url, options, successCallback, failCallback) => {
 	try {
-		const response = await fetch(url, {method: `post`, body: body});
+		const response = await fetch(url, {
+			method : `post`,
+			headers : options.headers,
+			body : options.body
+		});
 
+		// Check if response is ok
 		if (response.ok) {
-			successCallback ({
-				response: response,
-				text: await response.clone().text(),
-				json: await response.clone().json(),
-				blob: await response.clone().blob()
+			successCallback({
+				response,
+				json : await response.clone().json(),
+				text : await response.clone().text(),
+				blob : await response.blob()
 			});
 		} else {
-			failCallback (response);
+			failCallback(response);
 		}
 	} catch (error) {
-		failCallback (error);
+		failCallback(error);
 	}
 };
 
-// put data
-const putData = async (url, body, successCallback, failCallback) => {
+// Put data
+const putData = async (url, options, successCallback, failCallback) => {
 	try {
-		const response = await fetch(url, {method: `put`, body: body});
+		const response = await fetch(url, {
+			method : `put`,
+			headers : options.headers,
+			body : options.body
+		});
 
+		// Check if response is ok
 		if (response.ok) {
-			successCallback ({
-				response: response,
-				text: await response.clone().text(),
-				json: await response.clone().json(),
-				blob: await response.clone().blob()
+			successCallback({
+				response,
+				json : await response.clone().json(),
+				text : await response.clone().text(),
+				blob : await response.blob()
 			});
 		} else {
-			failCallback (response);
+			failCallback(response);
 		}
 	} catch (error) {
-		failCallback (error);
+		failCallback(error);
 	}
 };
 
-// delete data
-const deleteData = async (url, body, successCallback, failCallback) => {
+// Delete data
+const deleteData = async (url, options, successCallback, failCallback) => {
 	try {
-		const response = await fetch(url, {method: `delete`, body: body});
+		const response = await fetch(url, {
+			method : `delete`,
+			headers : options.headers,
+			body : options.body
+		});
 
+		// Check if response is ok
 		if (response.ok) {
-			successCallback ({
-				response: response,
-				text: await response.clone().text(),
-				json: await response.clone().json(),
-				blob: await response.clone().blob()
+			successCallback({
+				response,
+				json : await response.clone().json(),
+				text : await response.clone().text(),
+				blob : await response.blob()
 			});
 		} else {
-			failCallback (response);
+			failCallback(response);
 		}
 	} catch (error) {
-		failCallback (error);
+		failCallback(error);
 	}
 };
